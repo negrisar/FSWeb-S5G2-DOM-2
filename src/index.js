b5689ace149330a053cec5aec31deae542ac2ddd
Nergis <armaganergis@gmail.com>
2023-05-24 14:55:23 +0300

    çözümler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { buildQueries } from '@testing-library/dom';
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
@@ -7,3 +8,27 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+document.querySelectorAll("img").forEach(resim => {
+    resim.addEventListener("mouseover",(event) =>{
+        event.target.style.filter="grayscale(100%)"
+    })
+    resim.addEventListener("mouseout",(event) =>{
+        event.target.style.filter="grayscale(0%)"
+    })
+})
+
+window.addEventListener("load",(event)=>{
+    setTimeout(()=>{
+        document.querySelector("body").style.background = "purple"
+    }, "2000")
+})
+
+document.querySelectorAll("h2").forEach(baslik=>{
+    baslik.addEventListener("click",(event) =>{
+        event.target.style.color = "blue";
+    })
+    baslik.addEventListener("dblclick",(event) =>{
+        event.target.style.color = "black"
+    })
+})
+
